c45be3c094b7f51aa9076e0712b4f38d3bc6c801
Sakkarin Saysai <oat.saysai@gmail.com>
2024-03-26 21:46:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Read Credit Card Statement
+
+### Install Dependencies
+
+```sh
+python3 -m pip install pypdf
+python3 -m pip install pandas
+python3 -m pip install plotly
+python3 -m pip install cli-args-system
+```
+
+### How to run
+
+```sh
+python3 main.py -f KBGC_1000000047986508_240225.pdf -p 11111111
+```
---
+++ main.py
@@ -0,0 +1,87 @@
+from pypdf import PdfReader
+import re
+import pandas as pd
+import plotly.graph_objects as go
+from cli_args_system import Args
+
+args = Args(convert_numbers=False)
+filename = args.flag_str("f", "filename", "")
+password = args.flag_str("p", "password", "")
+
+# Read PDF and convert to temp.csv
+reader = PdfReader(stream=filename, password=password)
+
+f = open("temp.csv", "w")
+stop = False
+
+for page in reader.pages:
+    data = page.extract_text()
+    for lineNum, line in enumerate(data.split("\n")):
+
+        line = line.split("วันที่สรุปยอด", 1)[0]
+
+        if "PREVIOUS BALANCE" in line:
+            f.write(
+                "||PREVIOUS BALANCE||" + line.split(" ")[2].replace(",", "", -1) + "\n"
+            )
+
+        pattern = "[0-9]{2}/[0-9]{2}/[0-9]{2} [0-9]{2}/[0-9]{2}/[0-9]{2}"
+        if re.search(pattern, line):
+
+            tmp = ""
+
+            colInLine = line.split(" ")
+            for i, x in enumerate(colInLine):
+                if i <= 1:
+                    f.write(x + "|")
+                elif i == len(colInLine) - 3:
+                    if ":" in x:
+                        tmp = x
+                    else:
+                        f.write(x)
+                elif i == len(colInLine) - 2 or i == len(colInLine) - 1:
+                    if tmp != "":
+                        x = tmp + x
+                        tmp = ""
+                    f.write("|" + x.replace(",", "", -1))
+                else:
+                    f.write(x + " ")
+
+            f.write("\n")
+
+        if "TOTAL BALANCE" in line:
+            f.write(
+                "||TOTAL BALANCE||" + line.split(" ")[4].replace(",", "", -1) + "\n"
+            )
+            stop = True
+            break
+
+    if stop:
+        break
+
+f.close()
+
+# Read the CSV file into a DataFrame
+colNames = ["TRANS DATE", "POSTING DATE", "DESCRIPTION", "REFERENCE", "AMOUNT"]
+df = pd.read_csv("temp.csv", delimiter="|", names=colNames, header=None)
+
+df["AMOUNT"] = pd.to_numeric(df["AMOUNT"])
+
+# Remove some rows
+df = df.drop(df[df["DESCRIPTION"] == "PAYMENT - THANK YOU - MOB"].index)
+df = df.drop(df[df["DESCRIPTION"] == "PREVIOUS BALANCE"].index)
+df = df.drop(df[df["DESCRIPTION"] == "TOTAL BALANCE"].index)
+
+dfg = df.groupby("DESCRIPTION")["AMOUNT"].sum().sort_values(ascending=True)
+
+fig = go.Figure(
+    go.Bar(
+        x=dfg,
+        y=dfg.index,
+        text=dfg,
+        texttemplate="%{text:.2f}",
+        orientation="h",
+    )
+)
+
+fig.show()
